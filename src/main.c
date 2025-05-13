3fa7c351478ab3b64ba3b8bcf6db1c753696b283
haolipeng <1078285863@qq.com>
2025-05-13 18:23:39 +0800

    first commit

---
+++ src/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dpdk_capture.h"
+#include "plugin_framework.h"
+
+/**
+ * NetDefender主程序入口
+ * 基于DPDK的高性能网络入侵检测系统
+ */
+int main(int argc, char *argv[])
+{
+    printf("Hello World! 欢迎使用NetDefender - 基于DPDK的高性能网络入侵检测系统\n");
+    
+    // TODO: 初始化DPDK环境
+    
+    // TODO: 配置和启动数据包捕获
+    
+    // TODO: 初始化检测引擎
+    
+    // TODO: 主事件循环
+    
+    return 0;
+}
